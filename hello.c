a0db74be528925c76b9da86253e91930bf226366
hsu2 <59965884+hsu2@users.noreply.github.com>
2020-01-16 09:16:32 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* "Hello World" , My Hack01
+Name: Haiwen Su
+Collaborators: Joseph almutawa
+Date: 2020/02/16
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Haiwen Su\n");
+	return 0;
+}
